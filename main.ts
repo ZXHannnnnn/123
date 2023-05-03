a3fb359a61d3bab863e85c128507d09a4b3cd1f6
ZXHannnnnn <131329190+ZXHannnnnn@users.noreply.github.com>
2023-05-03 21:32:25 +0800

    dice

--- main.ts
+++ main.ts
@@ -1,3 +1,54 @@
-basic.forever(function () {
-	
+let dice = 0
+input.onGesture(Gesture.Shake, function () {
+    dice = randint(1, 6)
+    basic.showNumber(dice)
+    if (dice == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (dice == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (dice == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (dice == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (dice == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . . . . .
+            . # # # .
+            . . . . .
+            `)
+    }
 })
